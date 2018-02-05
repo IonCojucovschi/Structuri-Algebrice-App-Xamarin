42fe6e9d347071ebdf53807f290d80878fc61512
MyPC\Ion.Cojucovschi <i.cojucovschi@software-dep.net>
2018-02-06 00:19:00 +0200

    pdf viwewr test ,,,,run welll

---
+++ StructureAlgebrics/StructureAlgebrics/Pages/PdfViewer.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Com.Joanzapata.Pdfview;
+
+namespace StructureAlgebrics.Pages
+{
+    [Activity(Label = "Pdf Viewer", MainLauncher = true)]
+    public class PdfViewer : Activity
+    {
+        private PDFView pdfView;
+        private string resourcePdf;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.pdfView);
+
+            pdfView = FindViewById<PDFView>(Resource.Id.pdfView);
+            pdfView.FromAsset("Laborator3.pdf").Load();
+        }
+    }
+}
